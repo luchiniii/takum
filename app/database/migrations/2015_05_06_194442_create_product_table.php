c0eb274e5e97ad64a662d511620def99a21d2aed
Luis Aurelio Palomá Meza <luispaloma3@gmail.com>
2015-05-06 18:12:41 -0500

    Primer commit

---
+++ app/database/migrations/2015_05_06_194442_create_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('product', function ($table)
+		{
+			$table->increments('id');
+			$table->string('titulo');
+			$table->text('descripcion');
+			$table->softDeletes();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('product');
+	}
+
+}
